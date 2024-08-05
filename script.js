d185d2a6b11284ac900b62cb5a3be365bbc42392
JesiLyss <antonella.jesica.gomez@gmail.com>
2024-08-05 10:49:00 -0300

    fix responsive menu

--- script.js
+++ script.js
@@ -32,3 +32,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const menu = document.getElementById('menu');
+
+    menuIcon.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
